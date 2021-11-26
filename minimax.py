fa35beaddab809c72766189c30d7861c6447546f
Mikolaj Olejnik <olejnikmikolaj@gmail.com>
2021-11-26 16:17:22 +0100

    somehow working

--- minimax.py
+++ minimax.py
@@ -1,13 +1,14 @@
 
+from copy import deepcopy
 from games.dots_and_boxes import DotsAndBoxes, DotsAndBoxesMove
 
-def minimax(position, depth, alpha, beta, maximizingPlayer):
+def minimax(position, depth, maximizingPlayer, alpha=-float('inf'), beta=float('inf')):
     if depth == 0:
         return position.get_value()
 
     if maximizingPlayer:
         value = -float('inf')
-        for move in position.legal_moves():
+        for move in position.get_moves():
             value = max(value, minimax(move, depth - 1, alpha, beta, False))
             alpha = max(alpha, value)
             if beta <= alpha:
@@ -16,7 +17,7 @@ def minimax(position, depth, alpha, beta, maximizingPlayer):
 
     if not maximizingPlayer:
         value = float('inf')
-        for move in position.legal_moves():
+        for move in position.get_moves():
             value = min(value, minimax(move, depth - 1, alpha, beta, True))
             beta = min(beta, value)
             if beta <= alpha:
@@ -29,37 +30,49 @@ class Position:
         self.game = game
         self.value = None
         self.childs = []
+        self.depth = 0
         self.add_childs()
-    
-    def add_child(self, child):
-        self.childs.append(child)
-    
+        
     def add_childs(self):
-        print(self.game)
-        print(self.game.is_finished())
-        print(self.game.get_moves())
         if self.game.is_finished() == False:
             for move in self.game.get_moves():
-                game_copy = self.game
+                game_copy = deepcopy(self.game)
                 game_copy.make_move(move)
-                self.add_child(Position(game_copy))
+                self.childs.append(Position(game_copy))
+                if self.childs[-1].depth + 1 > self.depth:
+                    self.depth = self.childs[-1].depth + 1
         else:
-            if self.game.get_winner() == None:
+            self.depth = 0
+            winner = self.game.get_winner()
+            if winner == None:
                 self.value = 0
-            elif self.game.get_winner().char == '1':
+            elif winner.char == '1':
                 self.value = 1
-            else:
+            elif winner.char == '2':
                 self.value = -1
+    
+    def get_value(self):
+        return self.value
+
+    def get_moves(self):
+        return self.childs
 
 game = DotsAndBoxes(2)
 game.make_move(DotsAndBoxesMove('v', (0, 0)))
-game.make_move(DotsAndBoxesMove('h', (0, 0)))
-game.make_move(DotsAndBoxesMove('v', (1, 0)))
-game.make_move(DotsAndBoxesMove('h', (1, 2)))
-game.make_move(DotsAndBoxesMove('h', (0, 2)))
 game.make_move(DotsAndBoxesMove('v', (1, 2)))
+game.make_move(DotsAndBoxesMove('v', (1, 0)))
 game.make_move(DotsAndBoxesMove('v', (0, 2)))
-game.make_move(DotsAndBoxesMove('h', (1, 0)))
-game.make_move(DotsAndBoxesMove('h', (1, 1)))
+
+print(game, '\n')
 
 positions = Position(game)
+
+starting_player = True if positions.game.get_current_player().char == '1' else False
+result = minimax(positions, positions.depth, starting_player)
+print(result)
+if result == 1:
+    print('Player 1 wins')
+elif result == -1:
+    print('Player 2 wins')
+else:
+    print('It\'s a draw!')
